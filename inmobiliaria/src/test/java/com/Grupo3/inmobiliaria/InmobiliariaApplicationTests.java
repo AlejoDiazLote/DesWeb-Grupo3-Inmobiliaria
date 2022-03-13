ceac9b3a3c4b342e2b6baed8602476d1e6becd18
AlejoDiazLote <Alejandrodiazlote@gmail.com>
2022-03-13 18:07:14 -0500

    base

---
+++ inmobiliaria/src/test/java/com/Grupo3/inmobiliaria/InmobiliariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Grupo3.inmobiliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmobiliariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
